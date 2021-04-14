1065a7e1d631dc8e504c89f66d937bef9d11828a
charles Varela <cvare009@wch136-21.cs.ucr.edu>
2021-04-13 23:37:51 -0700

    finish part 2

--- Lab3-BitManip/test/tests.py
+++ Lab3-BitManip/test/tests.py
@@ -17,15 +17,15 @@
 tests = [ {'description': 'PINA: 0x00 => PORTC = 0x40',
     'steps': [ {'inputs': [('PINA',0x00)], 'iterations': 1 } ],
     'expected': [('PORTC',0x40)]},
-    {'description': 'PINA: 0x01 => PORTC = 0x60',
-    'steps': [ {'inputs': [('PINA',0x01)],'iterations': 1}], # Set PIN to val then run one iteration
-    'expected': [('PORTC',0x60)]}, 
-    {'description': 'PINA: 0x02 => PORTC = 0x60',
-    'steps': [ {'inputs': [('PINA',0x02)],'iterations': 1}], # Set PIN to val then run one iteration
-    'expected': [('PORTC',0x60)]},   
+    {'description': 'PINA: 0x83 => PORTC = 0x70',
+    'steps': [ {'inputs': [('PINA',0x83)],'iterations': 1}], # Set PIN to val then run one iteration
+    'expected': [('PORTC',0x70)]}, 
+    {'description': 'PINA: 0x07 => PORTC = 0x3C',
+    'steps': [ {'inputs': [('PINA',0x07)],'iterations': 1}], # Set PIN to val then run one iteration
+    'expected': [('PORTC',0x3C)]},   
 	]
 # Optionally you can add a set of "watch" variables these need to be global or static and may need
 # to be scoped at the function level (for static variables) if there are naming conflicts. The 
 # variables listed here will display everytime you hit (and stop at) a breakpoint
-watch = ['tmpA']
+watch = ['tmpA','tmpC']
 
